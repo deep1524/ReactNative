91bd91ebad65781394eeae8f300d8b5939614f1f
Deepkamal yadav <dkits1995@gmail.com>
2023-04-25 14:30:40 +0530

    make grid final

--- components/MakeGrid.jsx
+++ components/MakeGrid.jsx
@@ -1,44 +1,137 @@
 // Make Dynamic Grid
 
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-
+import { StyleSheet, Text, View,ScrollView} from 'react-native';
+const user = [
+    {
+      id: 1,
+      name: 'DEEP',
+    },
+    {
+      id: 2,
+      name: 'DEEP Yadav',
+    },
+    {
+      id: 3,
+      name: 'Pooja',
+    },
+    {
+      id: 4,
+      name: 'Dk',
+    },
+    {
+      id: 4,
+      name: 'Dk',
+    },
+    {
+      id: 4,
+      name: 'Dk',
+    },
+    {
+      id: 4,
+      name: 'Dk',
+    },
+    {
+      id: 4,
+      name: 'Dk',
+    },
+    {
+      id: 4,
+      name: 'Dk',
+    },
+    {
+      id: 4,
+      name: 'Dk',
+    },
+    {
+      id: 4,
+      name: 'Dk',
+    },
+    {
+      id: 4,
+      name: 'Dk',
+    },
+    {
+      id: 8,
+      name: 'Dk',
+    },
+    {
+      id: 8,
+      name: 'Dk',
+    },
+    {
+      id: 5,
+      name: 'Dk Yadav',
+    },
+    {
+      id: 6,
+      name: 'Dk Yadav',
+    },
+    {
+      id: 6,
+      name: 'anil',
+    },
+    {
+        id: 6,
+        name: 'anil',
+      },
+      {
+        id: 6,
+        name: 'anil',
+      },
+      {
+        id: 6,
+        name: 'anil',
+      },
+      {
+        id: 6,
+        name: 'anil',
+      },
+      {
+        id: 6,
+        name: 'anil',
+      },
+      {
+        id: 6,
+        name: 'anil',
+      },
+      {
+        id: 6,
+        name: 'anil',
+      },
+      {
+        id: 6,
+        name: 'anil',
+      },
+  ];
 const MakeGrid = () => {
   return (
     <>
       <Text>MAke Dynamic Grid</Text>
+      <ScrollView>
       <View style={{flex: 1, flexDirection: 'row', flexWrap: 'wrap'}}>
-        <Text style={styles.text}>deep </Text>
-        <Text style={styles.text}>deep </Text>
-        <Text style={styles.text}>deep </Text>
-        <Text style={styles.text}>deep </Text>
-        <Text style={styles.text}>deep </Text>
-        <Text style={styles.text}>deep </Text>
-        <Text style={styles.text}>deep </Text>
-        <Text style={styles.text}>deep </Text>
-        <Text style={styles.text}>deep </Text>
-        <Text style={styles.text}>deep </Text>
-        <Text style={styles.text}>deep </Text>
-        <Text style={styles.text}>deep </Text>
-        <Text style={styles.text}>deep </Text>
-        <Text style={styles.text}>deep </Text>
-        <Text style={styles.text}>deep </Text>
-        <Text style={styles.text}>deep </Text>
+         
+     {
+        user.map((item)=><Text style={styles.text}>{item.name}</Text>)
+     }
+    
       </View>
+      </ScrollView> 
     </>
   );
 };
 const styles = StyleSheet.create({
   text: {
-    width: 50,
-    height: 50,
+    
+    width: 100,
+    height: 100,
     borderColor: 'red',
     borderWidth: 2,
-    padding: 5,
+    padding: 15,
     textAlign: 'center',
     textAlignVertical: 'center',
     backgroundColor: 'blue',
-    margin: 5,
+    margin: 10,
   },
 });
 
