77b075ee894a38e53ff0750d8b8f6769b0fa24f5
Deepkamal yadav <dkits1995@gmail.com>
2023-04-27 17:49:43 +0530

    Component Did Mount and Component DidUpdate

--- components/Use_effect.jsx
+++ components/Use_effect.jsx
@@ -3,15 +3,20 @@ import { Button, Text, View } from 'react-native'
 
 const Use_effect = () => {
     const [count,setCount]=useState(0);
+    const [data,setData]=useState(100);
     useEffect(()=>{
 console.warn("hello")
-    },[])
+     },[data]) 
+    //  single array pass (component did mount)
+    // if you pass the any state in array then it will be componnt Didupdate
   return (
    <View>
     <Text>Use Effect</Text>
-    <Text>{count}</Text>
+    <Text>component DidMount:  {count}</Text>
+    <Text> Component DidUpdate:  {data}</Text>
 
     <Button title='click' onPress={()=>setCount(count+1)}></Button>
+    <Button title='click' onPress={()=>setData(data+1)}></Button>
    </View>
   )
 }
