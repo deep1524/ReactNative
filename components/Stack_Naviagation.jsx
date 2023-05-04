c6ce2d9fea512761f001b6cd494e479a648f36a1
Deepkamal yadav <dkits1995@gmail.com>
2023-05-04 09:39:53 +0530

    stack navigation style

--- components/Stack_Naviagation.jsx
+++ components/Stack_Naviagation.jsx
@@ -1,21 +1,42 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-import {NavigationContainer} from "@react-navigation/native"
-import {createNativeStackNavigator} from "@react-navigation/native-stack"
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './Page/Home';
 import Login from './Page/Login';
-const Stack= createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
 
 const Stack_Naviagation = () => {
   return (
     <NavigationContainer>
-        <Stack.Navigator>
-        <Stack.Screen name='Login' component={Login}/>
-          <Stack.Screen name='Home' component={Home}/>
-          
-        </Stack.Navigator>
+      <Stack.Navigator
+        // this is for all the stack navigation
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: 'red',
+          },
+          headerTitleStyle: {
+            fontSize: 20,
+          },
+          headerTintColor: 'green',
+        }}>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{
+            title: 'User Login',
+            headerStyle: {
+              backgroundColor: 'green',
+            },
+            headerTitleStyle: {
+              fontSize: 25,
+            },
+            headerTintColor: 'white',
+          }}
+        />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
     </NavigationContainer>
-  
   );
 };
 
