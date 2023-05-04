c3fb80210be910c36451ed5d31d0945b10a46a5c
Deepkamal yadav <dkits1995@gmail.com>
2023-05-04 10:05:30 +0530

    stack navigation search and button component

---
+++ components/Page/Search.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { TextInput } from 'react-native'
+
+const Search = () => {
+  return (
+    <TextInput placeholder='search your name' style={{fontSize:15,borderColor:"black",borderWidth:2}}/>
+  )
+}
+
+export default Search;
--- components/Stack_Naviagation.jsx
+++ components/Stack_Naviagation.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Alert, Button, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './Page/Home';
 import Login from './Page/Login';
+import Search from './Page/Search';
 const Stack = createNativeStackNavigator();
 
 const Stack_Naviagation = () => {
+  const leftbtnaction=()=>{
+   console.warn("left btn action")
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -24,6 +29,8 @@ const Stack_Naviagation = () => {
           name="Login"
           component={Login}
           options={{
+            headerLeft:()=><Button title='Left' onPress={leftbtnaction}></Button>,
+            headerRight:()=><Search/>,
             title: 'User Login',
             headerStyle: {
               backgroundColor: 'green',
