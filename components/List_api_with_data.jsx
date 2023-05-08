2e4e877705d2d2e9ac0d207326630c8d38a11f3a
Deepkamal yadav <dkits1995@gmail.com>
2023-05-08 18:42:39 +0530

    input validation

---
+++ components/List_api_with_data.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const List_api_with_data = () => {
+  return (
+    <View>
+        <Text>List_api_with_data</Text>List_api_with_data</View>
+  )
+}
+
+export default List_api_with_data
