35b3b86396e5a8efd18d7817c076e5d6cb413688
Deepkamal yadav <dkits1995@gmail.com>
2023-05-12 22:21:15 +0530

    add

--- components/List_api_with_data.jsx
+++ components/List_api_with_data.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Text, View } from 'react-native'
-// data api with data
+// data api with data lets do it tomarrow
 const List_api_with_data = () => {
   return (
     <View>
