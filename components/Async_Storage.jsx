23478aac0d2fd7fb12d7a64fff41053f0007594a
Deepkamal yadav <dkits1995@gmail.com>
2023-05-15 22:07:45 +0530

    asyncstorage

--- components/Async_Storage.jsx
+++ components/Async_Storage.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {Button, Text, View} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-// async storage
+// async storage implementation
 const Async_Storage = () => {
   const [user, setUser] = useState('');
   const setData = async () => {
